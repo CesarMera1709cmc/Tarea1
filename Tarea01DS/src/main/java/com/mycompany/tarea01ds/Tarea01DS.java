0b07ff74d7394c31eea5993a7d92955163b8a37d
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-11-08 00:26:00 -0500

    Commit Inicial

---
+++ Tarea01DS/src/main/java/com/mycompany/tarea01ds/Tarea01DS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea01ds;
+
+/**
+ *
+ * @author cesar
+ */
+public class Tarea01DS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
